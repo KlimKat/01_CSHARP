9035ce19e772640063eb28ab8a198426f4e7a4c8
Katherine Gabova <katerinagabova1997@gmail.com>
2024-03-17 11:16:46 +0300

    Created program for Task3

--- seminar2/Task3/Program.cs
+++ seminar2/Task3/Program.cs
@@ -5,3 +5,20 @@
 // 16, 8 => да
 // 4, 3 => нет, 1
 
+Console.WriteLine("input first digit");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input second digit");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+int result = digit1 % digit2;
+
+if (result == 0)
+{
+    Console.WriteLine("первое число кратно второму");
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("первое число не кратно второму");
+    Console.WriteLine(result);
+}
