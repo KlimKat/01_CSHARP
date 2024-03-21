49c2baf07619599bc479481e0b7599e4a68bc42d
Katherine Gabova <katerinagabova1997@gmail.com>
2024-03-21 14:31:03 +0300

    Added hw2/Task2 and created program for Task2

---
+++ hw2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: 
+// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 
+// и выдаёт номер координатной четверти плоскости, 
+// в которой находится эта точка.
+// Пример
+// 2,  3    =>   1
+// -5, 3    =>   2
+// -3, -2   =>   3
+// 4, -2    =>   4
+
+Console.WriteLine("Введите x:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine(1);
+else if (x < 0 && y > 0) Console.WriteLine(2);
+else if (x < 0 && y < 0) Console.WriteLine(3);
+else if (x > 0 && y < 0) Console.WriteLine(4);
+else Console.WriteLine("Неправильный ввод");
