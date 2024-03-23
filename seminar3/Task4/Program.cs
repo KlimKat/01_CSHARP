f796d05167b0c11e900a761482a94df800decc36
Katherine Gabova <katerinagabova1997@gmail.com>
2024-03-23 12:07:03 +0300

    Added seminar3/Task1, Task2, Task3, Task4

---
+++ seminar3/Task4/Program.cs
@@ -0,0 +1,65 @@
+﻿// Дано натуральное трёхзначное число. 
+// Создайте массив, состоящий из цифр этого числа. 
+// Младший разряд числа должен располагаться на 0- м 
+// индексе массива, старший – на 2-м.
+// Пример
+// 456  =>  [6 5 4]
+// 781  =>  [1 8 7]
+
+//Код работает, просто он с else, а ниже код с return
+// Console.WriteLine("Введите трёхзначное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number < 100 || number > 999)
+// {
+//     Console.WriteLine("Некорректный ввод");
+// }
+// else
+// {
+//     int[] result = new int[3];
+
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = number % 10;
+//         number /= 10;
+//     }
+
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         Console.Write(result[i] + " ");
+//     }
+// }
+
+
+
+
+
+
+//Или вариант без else но с return
+//Ввод числа
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Проверка числа
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+//Логика кода
+//Создаём массив
+    int[] result = new int[3];
+//Проходимся по массиву
+    for (int i = 0; i < result.Length; i++)
+    {
+        //кладём последний цифру в массив
+        result[i] = number % 10;
+        //отрезаем последнюю цифру у числа
+        number /= 10;
+    }
+//Выводим массив
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write(result[i] + " ");
+    }
+    
